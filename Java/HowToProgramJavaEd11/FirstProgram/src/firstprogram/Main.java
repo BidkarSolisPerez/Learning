c6f81ca313ccaa88aa598e417c46673566612087
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-01-04 09:43:56 -0600

    JavaExercises chap1
    JavaExercises chap1

---
+++ Java/HowToProgramJavaEd11/FirstProgram/src/firstprogram/Main.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstprogram;
+
+/**
+ *
+ * @author bisolisp
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Answer for chapter 1
+        /*
+        Self-Review Exercises
+        1.1 Fill in the blanks in each of the following statements:
+        Computers process data under the control of sets of instructions called programs.
+
+        The key logical units of the computer are the input unit, output unit, CPU, ALU, memory unit and secondary memory.
+
+        The three types of languages discussed in the chapter are Machine language, Assembly language and High level language.
+
+        The programs that translate high-level language programs into machine language are called compilers.
+
+        Android is an operating system for mobile devices based on the Linux kernel and Java.
+
+        Release candidates software is generally feature complete, (supposedly) bug free and ready for use by the community.
+
+        The Wii Remote, as well as many smartphones, use a(n) accelerometer which allows the device to respond to motion.
+
+        1.2 Fill in the blanks in each of the following sentences about the Java environment:
+
+        The java command from the JDK executes a Java application.
+
+        The javac 13command from the JDK compiles a Java program.
+
+        A Java source code file must end with the .java file extension.
+
+        When a Java program is compiled, the file produced by the compiler ends with the .class file extension.
+
+        The file produced by the Java compiler contains bytecodes that are executed by the Java Virtual Machine.
+
+        1.3 Fill in the blanks in each of the following statements (based on Section 1.5 ):
+
+        Objects enable the design practice of encapsulation —although they may know how to communicate with one another across well-defined interfaces, they normally are not allowed to know how other objects are implemented.
+
+        Java programmers concentrate on creating  classes, which contain fields and the set of methods that manipulate those fields and provide services to clients.
+
+        The process of analyzing and designing a system from an object-oriented point of view is called OOAD.
+
+        A new class of objects can be created conveniently by new reserved keyword—the new class (called the subclass) starts with the characteristics of an existing class (called the superclass), possibly customizing them and adding unique characteristics of its own.
+
+        Unified Modeling Language is a graphical language that allows people who design software systems to use an industry-standard notation to represent them.
+
+        The size, shape, color and weight of an object are considered attributes of the object’s class.
+        */
+    }
+    
+}
