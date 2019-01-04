3dafa1eda0bd2beb11e379e9d3de7c584f3f122d
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-01-04 11:09:34 -0600

    Update Main.java
    Second part of questions completion

--- Java/HowToProgramJavaEd11/FirstProgram/src/firstprogram/Main.java
+++ Java/HowToProgramJavaEd11/FirstProgram/src/firstprogram/Main.java
@@ -60,6 +60,65 @@ public class Main {
 
         The size, shape, color and weight of an object are considered attributes of the object’s class.
         */
+        
+        /*
+        1.4 Fill in the blanks in each of the following statements:
+
+        The logical unit that receives information from outside the computer for use by the computer is the Input unit.
+
+        The process of instructing the computer to solve a problem is called programming.
+
+        Assembly Language is a type of computer language that uses Englishlike abbreviations for machine-language instructions.
+
+        Output unit is a logical unit that sends information which has already been processed by the computer to various devices so that it may be used outside the computer.
+
+        Memory unit and storage unit are logical units of the computer that retain information.
+
+        Arithmetic is a logical unit of the computer that performs calculations.
+
+        Logic is a logical unit of the computer that makes logical decisions.
+
+        High level languages are most convenient to the programmer for writing programs quickly and easily.
+
+        The only language a computer can directly understand is that computer’s machine language.
+
+        CPU is a logical unit of the computer that coordinates the activities of all the other logical units.
+
+        1.5 Fill in the blanks in each of the following statements:
+
+        The Java programming language is now used to develop large-scale enterprise applications, to enhance the functionality of web servers, to provide applications for consumer devices and for many other purposes.
+
+        DOS initially became widely known as the development language of the UNIX operating system.
+
+        The TCP ensures that messages, consisting of sequentially numbered pieces called bytes, were properly routed from sender to receiver, arrived intact and were assembled in the correct order.
+
+        The C++ programming language was developed by Bjarne Stroustrup in the early 1980s at Bell Laboratories.
+
+        1.6 Fill in the blanks in each of the following statements:
+
+        Java programs normally go through five phases— edit, compile, load,verify and execute.
+
+        A(n) IDE provides many tools that support the software development process, such as editors for writing and editing programs, debuggers for locating logic errors in programs, and many other features.
+
+        The command java invokes the JVM, which executes Java programs.
+
+        A(n) Virtual machine is a software application that simulates a computer, but hides the underlying operating system and hardware from the programs that interact with it.
+
+        The class loader takes the .class files containing the program’s bytecodes and transfers them to primary memory.
+
+        The bytecode veryfier examines bytecodes to ensure that they’re valid.
+
+        1.7 Explain the two compilation phases of Java programs.
+        1 Phase: Sour code is translated to bytecode
+        2 Phase: Bytecode is translaged to mahine language which the code is currently running
+        1.8 One of the world’s most common objects is a wrist watch. Discuss how each of the following terms and concepts applies to the notion of a watch: object, attributes, behaviors, class, inheritance (consider, for example, an alarm clock), modeling, messages, encapsulation, interface and information hiding.
+        First as class is something generic and most of the time is an abstraction for some concept that is almost a subject in this case the main concept is watch
+        Is instrument that let us know the time and has some attributes and states like value of the current hour, minute and second. These states can be changed with some methods
+        and they call other methdods that process the changes and will be executed by another methods that encapsulate the funtionality of this.  
+        Now that the main concept has been applied we can create a different kind of watches, so class Watch just has the general classs definition and from them we can use inheritance
+        to create subclases like wrist Watches that it will take some new functionalities like alarms, sensors to measure presuare or some others functionalities that will be new classes
+        added to this new subclass from Watch class. In that way the world is just created from this Object Oriented Language paradigm.
+        */
     }
     
 }
